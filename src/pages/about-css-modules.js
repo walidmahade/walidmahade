b05bfa50c1c88fadf9325314b514be55e41cf319
walid <walid@walids-iMac.local>
2018-10-28 00:01:26 +0600

    Example nav menu with scss

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./about-css-modules.module.scss";
 import Container from "../components/container";
+import Nav from "../components/nav";
 
 console.log(styles);
 
@@ -15,19 +16,22 @@ const User = props => (
 );
 
 export default () => (
-    <Container>
-        <h1>About CSS Modules</h1>
-        <p>CSS Modules are cool</p>
-        <User
-            username="Jane Doe"
-            avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-            excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        />
+    <div className="App">
+        <Nav />
+        <Container>
+            <h1>About CSS Modules</h1>
+            <p>CSS Modules are cool</p>
+            <User
+                username="Jane Doe"
+                avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+                excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            />
 
-        <User
-            username="Bob Smith"
-            avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-            excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        />
-    </Container>
+            <User
+                username="Bob Smith"
+                avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+                excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            />
+        </Container>
+    </div>
 );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
+import Nav from "../components/nav";
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Link to="/">Home</Link>
-    <Header headerText="Contact" />
-    <p>Send us a message!</p>
-  </div>
-)
+    <div className="App">
+        <Nav />
+        <div style={{ color: `teal` }}>
+            <Link to="/">Home</Link>
+            <Header headerText="Contact" />
+            <p>Send us a message!</p>
+        </div>
+    </div>
+);
