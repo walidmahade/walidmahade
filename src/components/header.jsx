21cfebf1476d3c3e8480810cce78f95f47187fa4
walidmahade <walid_mahade@outlook.com>
2018-11-25 00:25:28 +0600

    dropdown added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import navStyles from "./header.module.scss";
+import "./header.scss";
 import "./styles/global.scss";
 import { Link } from "gatsby";
 import { Helmet } from "react-helmet";
@@ -7,89 +7,112 @@ import Logo from "../static/logo.png";
 import Favicon from "../static/favicon.ico";
 import Separator from "./global/separator";
 
-export default () => (
-    <header>
-        <nav className={navStyles.mainMenu}>
-            <Helmet>
-                {/* <link
-                    href="https://fonts.googleapis.com/css?family=Open+Sans:400|PT+Sans:700|PT+Serif"
-                    rel="stylesheet"
-                /> */}
-                <title>| Mahade Walid</title>
-                <link rel="shortcut icon" href={Favicon} />
-                <link
-                    rel="stylesheet"
-                    href="https://use.typekit.net/mny7oki.css"
-                />
-            </Helmet>
+export default class Header extends React.Component {
+    showDropdown = e => {
+        e.target.classList.add("showDropdown");
+        console.log(e.target.classList);
+    };
 
-            <div className={navStyles.brand}>
-                <Link to="/">
-                    <img src={Logo} alt="Mahade Walid" />
-                </Link>
-            </div>
+    hideDropdown = e => {
+        e.target.classList.remove("showDropdown");
+        console.log(e.target.classList);
 
-            <div className={navStyles.wrapper}>
-                <Link
-                    to="/"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Me
-                </Link>
-                <Link
-                    to="/portfolio"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Code
-                </Link>
-                <Link
-                    to="/objects"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Objects
-                </Link>
-                <Link
-                    to="/photo"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Photo
-                </Link>
-                <Link
-                    to="/video"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Video
-                </Link>
-                <Link
-                    to="/contact"
-                    className={navStyles.navItem}
-                    activeClassName={navStyles.active}
-                >
-                    Contact
-                </Link>
-                <Link
-                    to="/blog"
-                    className={navStyles.navItem + " " + navStyles.hasDropdown}
-                    activeClassName={navStyles.active}
-                >
-                    Blog
-                    <div className={navStyles.dropdown}>
-                        <Link to="/">Home</Link>
-                        <Link to="/">Home</Link>
-                        <Link to="/">Home</Link>
-                        <Link to="/">Home</Link>
+        let hasDropdown = document.querySelectorAll(".hasDropdown");
+
+        for (var i = 0; i < hasDropdown.length; i++) {
+            hasDropdown[i].classList.remove("showDropdown");
+        }
+    };
+
+    render() {
+        return (
+            <header>
+                <nav className="mainMenu">
+                    <Helmet>
+                        <title>| Mahade Walid</title>
+                        <link rel="shortcut icon" href={Favicon} />
+                        <link
+                            rel="stylesheet"
+                            href="https://use.typekit.net/mny7oki.css"
+                        />
+                    </Helmet>
+
+                    <div className="brand">
+                        <Link to="/">
+                            <img src={Logo} alt="Mahade Walid" />
+                        </Link>
                     </div>
-                </Link>
-            </div>
-        </nav>
 
-        <div className="mt-20 mb-35">
-            <Separator />
-        </div>
-    </header>
-);
+                    <div className="wrapper">
+                        <Link
+                            to="/"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Me
+                        </Link>
+                        <Link
+                            to="/portfolio"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Code
+                        </Link>
+                        <Link
+                            to="/objects"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Objects
+                        </Link>
+                        <Link
+                            to="/photo"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Photo
+                        </Link>
+                        <Link
+                            to="/video"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Video
+                        </Link>
+                        <Link
+                            to="/contact"
+                            className="navItem"
+                            activeClassName="active"
+                        >
+                            Contact
+                        </Link>
+                        <Link
+                            to="/blog"
+                            className="navItem hasDropdown"
+                            activeClassName="active"
+                            onMouseEnter={e => this.showDropdown(e)}
+                            onMouseLeave={e => this.hideDropdown(e)}
+                        >
+                            Blog
+                            <div
+                                className="dropdown"
+                                onMouseLeave={e => this.hideDropdown(e)}
+                            >
+                                <Link to="/">Home</Link>
+                                <Link to="/">About</Link>
+                                <Link to="/">Life</Link>
+                                <Link to="/">Lost</Link>
+                            </div>
+                        </Link>
+                    </div>
+                </nav>
+
+                <div className="mt-20 mb-35">
+                    <Separator />
+                </div>
+            </header>
+        );
+    }
+}
+
+// export default Header;
